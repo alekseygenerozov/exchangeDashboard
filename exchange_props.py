c0c252df7dda03f4532ee32c740d4fa624fa3af1
alekseygenerozov <aleksey.generozov@gmail.com>
2025-04-09 15:51:37 -0500

    Softening time and q with no halo mass.

--- exchange_props.py
+++ exchange_props.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import ks_2samp
 import matplotlib.patches as mpatches
+from starforge_mult_search.analysis.analyze_stack import subtract_path
+
 dummy_patch = mpatches.Patch(color='white', label='')
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import plotly.graph_objs as go
@@ -121,6 +123,15 @@ def apply_trinary_filter(condition, selection):
     elif selection == 'false':
         return ~condition
 
+def get_soft_time(id1, id2):
+    delta = subtract_path(path_lookup[id1][:, pxcol:pzcol + 1], path_lookup[id2][:, pxcol:pzcol + 1])
+    delta = np.sum(delta * delta, axis=1)**.5 * cgs.pc / cgs.au
+    idx_soft = np.where(delta < 40)[0]
+    if len(idx_soft) == 0:
+        return np.inf
+    else:
+        return idx_soft[0]
+
 ##NOT normal survival filter -- for now just look at things that survive as binaries.
 final_bin_filter = (my_data["final_bound_snaps_norm"]==1)
 quasi_filter = my_data["quasi_filter"]
@@ -129,26 +140,29 @@ pmult_before_bin = np.load("pmult_before_bin.npz")["pmult_filt"]
 exchange_filter = ~pmult_before_bin
 
 bprops = []
-
+my_data["soft_time"] = np.zeros(len(my_data["bin_ids"]))
 for ii, row in enumerate(my_data["bin_ids"]):
     bin_list = list(row)
-    sys1_info = lookup_dict[bin_list[0]]
-    sys2_info = lookup_dict[bin_list[1]]
+    id1 = bin_list[0]
+    id2 = bin_list[1]
+    sys1_info = lookup_dict[id1]
+    sys2_info = lookup_dict[id2]
     ##Snaps are all the snapshots the stars are in the same system -- not the snapshots they are in the same binary.
     b1, b2, snaps = get_bound_snaps(sys1_info, sys2_info)
     sel_bin_snap = int(my_data["final_bound_snaps"][ii])
-    spin_ang = [spin_angle_from_lookup(spin_lookup, bin_list[0], bin_list[1], ss) for ss in b1[:, LOOKUP_SNAP].astype(int)]
-    sim_end_snap = lookup_dict[bin_list[0]][0, -1]
+    spin_ang = [spin_angle_from_lookup(spin_lookup, id1, id2, ss) for ss in b1[:, LOOKUP_SNAP].astype(int)]
+    sim_end_snap = lookup_dict[id1][0, -1]
     ##Softening filter??
     ##get_first_soft_time--can be done separately...
-
+    q_no_halo =  np.min((b1[:, LOOKUP_M] / b2[:, LOOKUP_M], b2[:, LOOKUP_M] / b1[:, LOOKUP_M]), axis=0)
+    my_data["soft_time"][ii] = get_soft_time(id1, id2)
     ##Also have toggle for halo masses(!)
     ## Mass ratio without halos -- controlled by button. Need new column or can be reconstructed from masses...
     # bprops.append([b1[:, LOOKUP_SMA], b1[:, LOOKUP_ECC], np.min((b1[:, LOOKUP_Q], b2[:, LOOKUP_Q]), axis=0),
     #                b1[:, LOOKUP_SNAP] - b1[0,LOOKUP_SNAP], b1[:, LOOKUP_SNAP] / sim_end_snap, b1[:, LOOKUP_SNAP], np.ones(len(b1)) * ii, spin_ang,
     #                np.min((b1[:, LOOKUP_M] / b2[:, LOOKUP_M], b2[:, LOOKUP_M] / b1[:, LOOKUP_M]), axis=0)])
     bprops.append([b1[:, LOOKUP_SMA], b1[:, LOOKUP_ECC], np.min((b1[:, LOOKUP_Q], b2[:, LOOKUP_Q]), axis=0),
-                   b1[:, LOOKUP_SNAP] - b1[0,LOOKUP_SNAP], b1[:, LOOKUP_SNAP] / sim_end_snap,  np.ones(len(b1)) * ii, b1[:, LOOKUP_SNAP], spin_ang])
+                   b1[:, LOOKUP_SNAP] - b1[0,LOOKUP_SNAP], b1[:, LOOKUP_SNAP] / sim_end_snap,  np.ones(len(b1)) * ii, b1[:, LOOKUP_SNAP], spin_ang, q_no_halo])
 
 import pandas as pd
 bprops = pd.concat([pd.DataFrame(row).T for row in bprops], ignore_index=0)
@@ -172,9 +186,9 @@ mfilter.index.name = "bin_idx"
 mfilter.rename(columns={0:"mfinal_primary"}, inplace=True)
 
 ##TO DO: ADD THIS FILTER
-# soft_time = pd.DataFrame(my_data["soft_time"])
-# soft_time.index.name = "bin_idx"
-# soft_time.rename(columns={0:"soft_time"}, inplace=True)
+soft_time = pd.DataFrame(my_data["soft_time"])
+soft_time.index.name = "bin_idx"
+soft_time.rename(columns={0:"soft_time"}, inplace=True)
 
 seeds_lookup = pd.DataFrame(seeds_lookup)
 seeds_lookup.index.name = "bin_idx"
@@ -184,6 +198,7 @@ seeds_lookup.rename(columns={0:"seeds_lookup"}, inplace=True)
 bprops = pd.merge(bprops, qfilter, on="bin_idx", how="outer")
 bprops = pd.merge(bprops, efilter, on="bin_idx", how="outer")
 bprops = pd.merge(bprops, bfilter, on="bin_idx", how="outer")
+bprops = pd.merge(bprops, mfilter, on="bin_idx", how="outer")
 bprops = pd.merge(bprops, mfilter, on="bin_idx", how="outer")
 bprops = pd.merge(bprops, seeds_lookup, on="bin_idx", how="outer")
 
@@ -264,8 +279,8 @@ def update_graph(min_mass, a_ex, a_bfb, b_ex, b_bfb, snap_from_form, halo_toggle
     ##Add soft filter to traces1 and traces2
     ##by modifying the extra_filt(!) -- Need to get soft filter online first.
     ##extra_filt = extra_filt & (bprops["snap"] < bprops["soft"])
-    traces1 = quad_plot(bprops_filt, filt_a1 & filt_a2, filt_b1 & filt_b2, extra_filt=(extra_filt), type="sma", unit=cgs.pc / cgs.au, log=True)
-    traces2 = quad_plot(bprops_filt, filt_a1 & filt_a2, filt_b1 & filt_b2, extra_filt=(extra_filt), type="e")
+    traces1 = quad_plot(bprops_filt, filt_a1 & filt_a2, filt_b1 & filt_b2, extra_filt=(extra_filt & (bprops["snap"] < bprops["soft_time"])), type="sma", unit=cgs.pc / cgs.au, log=True)
+    traces2 = quad_plot(bprops_filt, filt_a1 & filt_a2, filt_b1 & filt_b2, extra_filt=(extra_filt & (bprops["snap"] < bprops["soft_time"])), type="e")
     traces3 = quad_plot(bprops_filt, filt_a1 & filt_a2, filt_b1 & filt_b2, extra_filt=(extra_filt), type=qtype)
     traces4 = quad_plot(bprops_filt, filt_a1 & filt_a2, filt_b1 & filt_b2, extra_filt=(extra_filt), type="spin_ang")
 
